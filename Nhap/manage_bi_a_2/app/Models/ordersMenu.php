4c9633210ca8c80b886824e7f80ccdab776f73ab
tung_duong <duongdtpa00194>
2025-04-14 19:41:37 +0700

    manage new

--- Nhap/manage_bi_a_2/app/Models/ordersMenu.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    namespace App\Models;
-    class ordersMenu extends Model {
-        public function deleteOrderMenu($id){
-            $sql = "DELETE FROM orders_menu WHERE session_id = :session_id";
-            $this->query($sql, ['session_id' => $id]);
-        }
-    }
-?>
